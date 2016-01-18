ace685fa87ff3e3fae5751688a70accf5c620707
Aubrey Ford <musicaubrey@gmail.com>
2016-01-17 23:07:26 -0700

    migration1

--- knexfile.js
+++ knexfile.js
@@ -1,1 +1,12 @@
+require('dotenv').load()
 
+module.exports = {
+development: {
+client: pg,
+connection: 'localDBLocation'
+},
+
+producton: {
+client:pg,
+connection: process.env.DATABASE_URL + ‘?ssl=true'
+}
---
+++ migrations/20160117230039_restaurants.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise){
+     return knex.schema.createTable('rests', function(table){
+          table.increments()
+          table.text('name')
+          table.text('location')
+          table.text('state')
+          table.text('cuisine')
+          table.integer('rating')
+          table.text('image')
+          table.text('bio')
+
+exports.down = function(knex, Promise){
+     return knex.schema.dropTable('rests')
